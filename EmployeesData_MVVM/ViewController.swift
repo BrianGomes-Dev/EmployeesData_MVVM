e56e137f2cadeea3d6c852df12c6c634adc5379e
BrianGomes <dev.briangomes@gmail.com>
2020-09-24 17:42:50 +0100

    Initial Commit

---
+++ EmployeesData_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmployeesData_MVVM
+//
+//  Created by user172197 on 9/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
